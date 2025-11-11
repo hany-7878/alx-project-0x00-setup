6c8cfc7da25677f5ca29a4fcf10745a1691e659d
hany-7878 <htesfaye641@gmail.com>
2025-11-11 02:07:16 -0800

    Setting up project directories

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,8 +1,19 @@
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <div style={{ textAlign: 'center', marginTop: '50px' }}>
+//       <h1>Welcome to Next.js (Pages Router)</h1>
+//       <p>This project uses the classic pages/ directory.</p>
+//     </div>
+//   )
+// }
+
+const Home: React.FC = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to Next.js (Pages Router)</h1>
-      <p>This project uses the classic pages/ directory.</p>
-    </div>
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
+    </main>
   )
 }
+
+export default Home;
