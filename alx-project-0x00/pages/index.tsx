b129a5557b12a7b0b666fef75b4f810c6aa69970
hany-7878 <htesfaye641@gmail.com>
2025-11-11 01:49:16 -0800

    setup

---
+++ alx-project-0x00/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Next.js (Pages Router)</h1>
+      <p>This project uses the classic pages/ directory.</p>
+    </div>
+  )
+}
