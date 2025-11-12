174da29428d6d2fb1973dd37198a2d11e6a4f599
hany-7878 <htesfaye641@gmail.com>
2025-11-12 10:56:02 -0800

    Rendering Components

---
+++ alx-project-0x00/components/Pill.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Define props for the component
+interface PillProps {
+  title: string;
+}
+
+// Accept props properly
+const Pill: React.FC<PillProps> = ({ title }) => {
+  return (
+    <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className="text-sm">{title}</p>
+    </div>
+  );
+};
+
+export default Pill;
